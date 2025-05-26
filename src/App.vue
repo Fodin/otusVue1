56816b1de2b64646307c37094d92334c022da987
Fedor Odintsov <fadmin@list.ru>
2025-05-26 22:04:55 +0300

    ДЗ Шаблоны

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,58 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { data as users } from "./data";
+import { ref } from "vue";
+
+const showUsers = ref(true);
+const hoveredUserId = ref<number | null>(null);
+</script>
 
 <template>
-  <div>OTUS</div>
+  <div class="wrapper">
+    <h1>ДЗ Шаблоны</h1>
+    <button @click="showUsers = !showUsers">
+      {{ showUsers ? "Скрыть список" : "Показать список" }}
+    </button>
+
+    <div v-if="showUsers">
+      <div v-for="user in users" :key="user.id">
+        <div
+          @mouseover="hoveredUserId = user.id"
+          @mouseleave="hoveredUserId = null"
+        >
+          <span class="name" :class="{ _hovered: hoveredUserId === user.id }">
+            {{ user.name }}
+          </span>
+          <span v-show="user.id % 3" class="email">
+            {{ " E-mail: " }}
+            {{ user.email }}
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.wrapper {
+  margin: 40px;
+  font-family: "Helvetica", sans-serif;
+}
+
+button {
+  margin-bottom: 10px;
+  padding: 5px 10px;
+}
+
+.name {
+  font-size: 20px;
+  line-height: 32px;
+}
+
+.email {
+  color: grey;
+}
+
+._hovered {
+  color: green;
+}
+</style>
